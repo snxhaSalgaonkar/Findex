d216e6dd916c2b7d0c074605fddfcee5e06f7495
Samidha2422 <samidharaul530@gmail.com>
2025-04-24 11:02:40 +0530

    newFile

---
+++ findex/lib/gitstart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Gitstart extends StatefulWidget {
+  const Gitstart({super.key});
+
+  @override
+  State<Gitstart> createState() => _GitstartState();
+}
+
+class _GitstartState extends State<Gitstart> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
